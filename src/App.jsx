a5e93f257260c6a477412baf6deb4bfdc3f5a365
developerAshish08 <sam.kingston.96@gmail.com>
2025-02-11 10:25:26 +0530

    done the filter section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Filter from "./Components/Filter";
 import Footer from "./Components/Footer";
 import React, { useState } from "react";
 import Header from "./Components/Header";
-import SearchBar from "./Components/SearchBar";
+// import SearchBar from "./Components/SearchBar";
 
 import CourseList from "./Components/CourseList";
 
@@ -56,40 +56,40 @@ import CourseDetailsPage from "./Pages/CourseDetailsPage";
 // ];
 
 const courses = [
-  { name: 'AMC Exam Preparation Course', category: 'Doctor', description: ' Clear the Australian Medical Council (AMC) Exam on your first attempt with the top-notch and best AMC coaching online. Our program features comprehensive online classes, extensive study materials, and AI-driven mock tests, making it the perfect AMC Exam Preparation Course for international doctors aiming to migrate to and practice in Australia. Enroll in our proven program today and pass the Australian Medical Council Exam with confidence and ease', brochure: './client/public/Assets/Courses/06-trinity college dublin.jpeg', fees: '1000 AUD', students: '1000+' , link:"#"},
-  { name: 'Pharmacy Exam', category: 'Pharmacist', description: ' Clear the Australian Medical Council (AMC) Exam on your first attempt with the top-notch and best AMC coaching online. Our program features comprehensive online classes, extensive study materials, and AI-driven mock tests, making it the perfect AMC Exam Preparation Course for international doctors aiming to migrate to and practice in Australia. Enroll in our proven program today and pass the Australian Medical Council Exam with confidence and ease', brochure: './client/public/Assets/Courses/06-trinity college dublin.jpeg', fees: '800 AUD', students: '800+' , link:"#"},
-  { name: 'Nursing Exam', category: 'Nurses', description: ' Clear the Australian Medical Council (AMC) Exam on your first attempt with the top-notch and best AMC coaching online. Our program features comprehensive online classes, extensive study materials, and AI-driven mock tests, making it the perfect AMC Exam Preparation Course for international doctors aiming to migrate to and practice in Australia. Enroll in our proven program today and pass the Australian Medical Council Exam with confidence and ease', brochure: './client/public/Assets/Courses/06-trinity college dublin.jpeg', fees: '700 AUD', students: '700+' , link:"#"},
-  { name: 'AMC Exam Preparation Course', category: 'Doctor', description: ' Clear the Australian Medical Council (AMC) Exam on your first attempt with the top-notch and best AMC coaching online. Our program features comprehensive online classes, extensive study materials, and AI-driven mock tests, making it the perfect AMC Exam Preparation Course for international doctors aiming to migrate to and practice in Australia. Enroll in our proven program today and pass the Australian Medical Council Exam with confidence and ease', brochure: './client/public/Assets/Courses/06-trinity college dublin.jpeg', fees: '1000 AUD', students: '1000+' , link:"#"},
-  { name: 'Pharmacy Exam', category: 'Pharmacist', description: ' Clear the Australian Medical Council (AMC) Exam on your first attempt with the top-notch and best AMC coaching online. Our program features comprehensive online classes, extensive study materials, and AI-driven mock tests, making it the perfect AMC Exam Preparation Course for international doctors aiming to migrate to and practice in Australia. Enroll in our proven program today and pass the Australian Medical Council Exam with confidence and ease', brochure: './client/public/Assets/Courses/06-trinity college dublin.jpeg', fees: '800 AUD', students: '800+' , link:"#"},
-  { name: 'Nursing Exam', category: 'Nurses', description: ' Clear the Australian Medical Council (AMC) Exam on your first attempt with the top-notch and best AMC coaching online. Our program features comprehensive online classes, extensive study materials, and AI-driven mock tests, making it the perfect AMC Exam Preparation Course for international doctors aiming to migrate to and practice in Australia. Enroll in our proven program today and pass the Australian Medical Council Exam with confidence and ease', brochure: './client/public/Assets/Courses/06-trinity college dublin.jpeg', fees: '700 AUD', students: '700+', link:"#" }
+  { name: 'AMC Exam Preparation Course', category: 'Doctor', description: ' Clear the Australian Medical Council (AMC) Exam on your first attempt with the top-notch and best AMC coaching online. Our program features comprehensive online classes, extensive study materials, and AI-driven mock tests, making it the perfect AMC Exam Preparation Course for international doctors aiming to migrate to and practice in Australia. Enroll in our proven program today and pass the Australian Medical Council Exam with confidence and ease', brochure: './client/public/Assets/Courses/06-trinity college dublin.jpeg', fees: '1000 AUD', students: '1000+', link: "#" },
+  { name: 'Pharmacy Exam', category: 'Pharmacist', description: ' Clear the Australian Medical Council (AMC) Exam on your first attempt with the top-notch and best AMC coaching online. Our program features comprehensive online classes, extensive study materials, and AI-driven mock tests, making it the perfect AMC Exam Preparation Course for international doctors aiming to migrate to and practice in Australia. Enroll in our proven program today and pass the Australian Medical Council Exam with confidence and ease', brochure: './client/public/Assets/Courses/06-trinity college dublin.jpeg', fees: '800 AUD', students: '800+', link: "#" },
+  { name: 'Nursing Exam', category: 'Nurses', description: ' Clear the Australian Medical Council (AMC) Exam on your first attempt with the top-notch and best AMC coaching online. Our program features comprehensive online classes, extensive study materials, and AI-driven mock tests, making it the perfect AMC Exam Preparation Course for international doctors aiming to migrate to and practice in Australia. Enroll in our proven program today and pass the Australian Medical Council Exam with confidence and ease', brochure: './client/public/Assets/Courses/06-trinity college dublin.jpeg', fees: '700 AUD', students: '700+', link: "#" },
+  { name: 'AMC Exam Preparation Course', category: 'Doctor', description: ' Clear the Australian Medical Council (AMC) Exam on your first attempt with the top-notch and best AMC coaching online. Our program features comprehensive online classes, extensive study materials, and AI-driven mock tests, making it the perfect AMC Exam Preparation Course for international doctors aiming to migrate to and practice in Australia. Enroll in our proven program today and pass the Australian Medical Council Exam with confidence and ease', brochure: './client/public/Assets/Courses/06-trinity college dublin.jpeg', fees: '1000 AUD', students: '1000+', link: "#" },
+  { name: 'Pharmacy Exam', category: 'Pharmacist', description: ' Clear the Australian Medical Council (AMC) Exam on your first attempt with the top-notch and best AMC coaching online. Our program features comprehensive online classes, extensive study materials, and AI-driven mock tests, making it the perfect AMC Exam Preparation Course for international doctors aiming to migrate to and practice in Australia. Enroll in our proven program today and pass the Australian Medical Council Exam with confidence and ease', brochure: './client/public/Assets/Courses/06-trinity college dublin.jpeg', fees: '800 AUD', students: '800+', link: "#" },
+  { name: 'Nursing Exam', category: 'Nurses', description: ' Clear the Australian Medical Council (AMC) Exam on your first attempt with the top-notch and best AMC coaching online. Our program features comprehensive online classes, extensive study materials, and AI-driven mock tests, making it the perfect AMC Exam Preparation Course for international doctors aiming to migrate to and practice in Australia. Enroll in our proven program today and pass the Australian Medical Council Exam with confidence and ease', brochure: './client/public/Assets/Courses/06-trinity college dublin.jpeg', fees: '700 AUD', students: '700+', link: "#" }
 ];
 function App() {
 
-   
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={
           <>
-           
-              <>
-                <FilterSection />
-                <SearchBar />
-                {/* <CourseList courses={courses}   /> */}
-              </>
-          
-          
-      
+
+            <>
+              <FilterSection />
+              {/* <SearchBar /> */}
+              {/* <CourseList courses={courses}   /> */}
+            </>
+
+
+
           </>
-        }/>
-        <Route path="/courses" element={<CourseList/>}/>
-        <Route path="/counselors" element={<CounselorList/>}/>
-       
+        } />
+        <Route path="/courses" element={<CourseList />} />
+        <Route path="/counselors" element={<CounselorList />} />
+
         <Route path="/preview/:name" element={<Filter />} />
-        <Route path="/:courseName" element={<CourseDetailsPage/>} />
+        <Route path="/:courseName" element={<CourseDetailsPage />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
