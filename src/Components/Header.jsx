9c8cc1bf4f946ea602ab7e61ca10ea71a7dd05a9
developerAshish08 <sam.kingston.96@gmail.com>
2025-04-12 15:05:01 +0530

    header fix

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
-import logo from "../../public/Assets/logo.svg"
+import logo from "/Assets/logo.svg"
 const Header = () => {
   const navigate = useNavigate();
 
