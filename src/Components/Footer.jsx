8b48561b7a556569560e7f8aa8e7b1e46a1c4542
Sachinnegi9936167436 <sachinnegi9936167436@gmail.com>
2026-07-20 09:55:25 +0530

    placements cards add

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-[#030A21] text-white p-4 text-center ">
-      &copy; 2025 Academically. All Rights Reserved.
+      &copy; 2026 Academically. All Rights Reserved.
     </footer>
   );
 };
